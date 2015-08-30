3e97de0d5ae288056202a95e249957a8ffa46092
hojahoja <jari.honkonen@helsinki.fi>
2015-08-30 13:58:18 +0300

    herokuun

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,8 @@
 var Sequelize = require('sequelize');
-
-var sequelize = new Sequelize('foorumi', '', '', {
-  dialect: 'sqlite',
-  storage: 'db/database.sqlite'
-});
+    var sequelize = new Sequelize('postgres://nyuposvqgrvysv:bZWr81DuntArzqz6nvZZ0Gzw7U@ec2-54-83-58-191.compute-1.amazonaws.com:5432/daqh95csbvsvap', {
+      dialect: 'postgres',
+      protocol: 'postgres'
+    });
 
 module.exports = {
   DataTypes: Sequelize,
